0c0175c64608c5dce84a1af7d1afd1b44998747b
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-04 14:17:38 +0000

    Sort old articles, make eyewitness story links in a visual way that is more like a stack of photos, #3041

--- eea/signals/browser/signals_view.py
+++ eea/signals/browser/signals_view.py
@@ -1,5 +1,7 @@
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+
 
 
 class SignalsView(BrowserView):
@@ -8,9 +10,23 @@ class SignalsView(BrowserView):
 
     @property
     def old_reports(self):
+        now = DateTime()
         catalog = getToolByName(self.context, 'portal_catalog')
         return catalog({
             'object_provides': 'eea.reports.interfaces.IReportContainerEnhanced',
             'review_state': 'published',
             'publication_groups': (u'environmental_assessment_report_2002_9',),
+            'effectiveRange': now,
+            'sort_on': 'effective',
+            'sort_order' : 'reverse',
         })
+
+    @property
+    def more_info_document(self):
+        obj = self.context.restrictedTraverse('more')
+        return {
+            'title': obj.getTitle(),
+            'description': obj.getDescription(),
+            'url': obj.absolute_url(),
+        }
+
