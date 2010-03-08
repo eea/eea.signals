2a86ec2a68a9ed1fb4ff63312ec1e516292de816
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-08 10:05:08 +0000

    Tagged eea.signals 0.2.

---
+++ trunk/eea.signals/tags/0.2/eea/signals/browser/signals_view.py
@@ -0,0 +1,62 @@
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+
+
+
+class SignalsView(BrowserView):
+    """ View to use within signals_view template.
+    """
+
+    def get_old_reports(self):
+        now = DateTime()
+        catalog = getToolByName(self.context, 'portal_catalog')
+        return catalog({
+            'object_provides': 'eea.reports.interfaces.IReportContainerEnhanced',
+            'review_state': 'published',
+            'publication_groups': (u'environmental_assessment_report_2002_9',),
+            'effectiveRange': now,
+            'sort_on': 'effective',
+            'sort_order' : 'reverse',
+        })
+
+    def get_chapters(self):
+        folder = self.context.restrictedTraverse('chapters')
+        contents = []
+        for brain in folder.getFolderContents({'portal_type': ['Article']}):
+            contents.append({
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+            })
+        return {
+            'folder_title': folder.Title(),
+            'folder_description': folder.Description(),
+            'folder_url': folder.absolute_url(),
+            'contents': contents,
+        }
+
+    def get_eyewitness_stories(self):
+        folder = self.context.restrictedTraverse('galleries')
+        contents = []
+        for brain in folder.getFolderContents({'portal_type': ['EyewitnessStory']}):
+            contents.append({
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+            })
+        return {
+            'folder_title': folder.Title(),
+            'folder_description': folder.Description(),
+            'folder_url': folder.absolute_url(),
+            'contents': contents,
+        }
+
+    def get_more_info_document(self):
+        obj = self.context.restrictedTraverse('what-is-signals')
+        return {
+            'title': obj.Title(),
+            'description': obj.Description(),
+            'url': obj.absolute_url(),
+        }
+
