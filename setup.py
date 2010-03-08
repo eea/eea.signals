2989d3b3224763be308ef498ba30e520a94878e1
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-08 10:05:02 +0000

    Use versions.txt to determine version number

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
+from os.path import join
 
-version = '0.1'
+name = 'eea.signals'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
 
 setup(name='eea.signals',
       version=version,
