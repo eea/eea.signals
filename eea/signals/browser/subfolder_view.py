1f17d0942198664d94034ed64baa757e1ad9fcbb
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-11 12:43:24 +0000

    More optimized solution that doesn't require to loop over the folder contents twice

--- eea/signals/browser/subfolder_view.py
+++ eea/signals/browser/subfolder_view.py
@@ -1,47 +1,45 @@
 from Products.Five import BrowserView
 
 
+def _get_contents(folder_brain):
+    """Get contents of folderish brain (cachable list/dict format)"""
+    if folder_brain.portal_type == 'Folder':
+        brains = folder_brain.getObject().getFolderContents()
+    elif folder_brain.portal_type in ['Topic', 'RichTopic']:
+        brains = folder_brain.getObject().queryCatalog()
+    return [{
+        'title': brain.Title,
+        'description': brain.Title,
+        'url': brain.getURL(),
+        'portal_type': brain.portal_type,
+    } for brain in brains]
+
+
 class SubFolderView(BrowserView):
     """ View that shows the contents of all subfolders in the context folder
     """
 
     @property
-    def subfolders(self):
+    def folder_contents(self):
         """Get the folderish items in cachable list/dict format"""
-        res = []
-        for folder in self.context.getFolderContents():
-            if folder.portal_type == 'Folder':
-                brains = folder.getObject().getFolderContents()
-            elif folder.portal_type in ['Topic', 'RichTopic']:
-                brains = folder.getObject().queryCatalog()
-            else:
-                continue
-            contents = []
-            for brain in brains:
-                contents.append({
-                    'title': brain.Title,
-                    'description': brain.Title,
-                    'url': brain.getURL(),
-                    'portal_type': brain.portal_type,
-                })
-            res.append({
-                'title': folder.Title,
-                'description': folder.Description,
-                'url': folder.getURL(),
-                'contents': contents,
-            })
-        return res
-
-    @property
-    def nonfolders(self):
-        """Get the non-folderish items in cachable list/dict format"""
-        res = []
+        ret = {
+            'folderish': [],
+            'nonfolderish': [],
+        }
         for brain in self.context.getFolderContents():
-            if brain.portal_type not in ['Folder', 'Topic', 'RichTopic']:
-                res.append({
+            if brain.portal_type in ['Folder', 'Topic', 'RichTopic']:
+                ret['folderish'].append({
                     'title': brain.Title,
                     'description': brain.Description,
-                    'url': brain.absolute_url,
+                    'url': brain.getURL(),
+                    'portal_type': brain.portal_type,
+                    'contents': _get_contents(brain),
+                })
+            else:
+                ret['nonfolderish'].append({
+                    'title': brain.Title,
+                    'description': brain.Description,
+                    'url': brain.getURL(),
                     'portal_type': brain.portal_type,
                 })
         return res
