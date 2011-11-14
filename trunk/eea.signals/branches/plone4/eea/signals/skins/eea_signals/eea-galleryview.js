4c610cba7bac9e1c1a4fe9b42fef8c665944efca
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-14 12:24:08 +0000

    Refs #8655; made the galleryview bigger for gallery_fancybox_view to cover the area in multimedia page; syncronized gallery_view changes from gallery_fancybox_view to gallery_view and made a macro out of it in gallery_view which is referenced in gallery_fancybox_view

--- trunk/eea.signals/branches/plone4/eea/signals/skins/eea_signals/eea-galleryview.js
+++ trunk/eea.signals/branches/plone4/eea/signals/skins/eea_signals/eea-galleryview.js
@@ -4,14 +4,21 @@
       to avoid namespace polution and conflicts with libraries that use $ */
 $(document).ready(function() {
     if ($.fn.galleryView !== undefined) {
-        $('#galleryView').galleryView({
-            panel_width: 635,
-            panel_height: 423,
-            frame_width: 50,
-            frame_height: 50,
-            transition_speed: 350,
-            transition_interval: 0
-        });
+        var $gallery_view = $('#galleryView');
+        if ($gallery_view.length ) {
+            var $gallery_parent = $gallery_view.parent();
+            var $gallery_class = $gallery_parent[0].className;
+            var gallery_width = $gallery_class === 'gallery_fancybox_view' ? 640 : 635;
+            var gallery_height = $gallery_class === 'gallery_fancybox_view' ? 433 : 423;
+            $('#galleryView').galleryView({
+                panel_width: gallery_width,
+                panel_height: gallery_height,
+                frame_width: 50,
+                frame_height: 50,
+                transition_speed: 350,
+                transition_interval: 0
+            });
+        }
     }
 });
 }(jQuery));
