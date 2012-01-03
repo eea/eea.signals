e2d56be98949eff708e354e20d6beff39e24552b
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-01-03 15:42:06 +0000

    Tagged eea.signals 4.2.

---
+++ trunk/eea.signals/tags/4.2/eea/signals/skins/eea_signals/eea-galleryview.js
@@ -0,0 +1,24 @@
+/* This JS integrates jquery.galleryview with the EEA site. */
+(function($) {
+    /* wrap code in self calling anonymous function with $ as parameter
+      to avoid namespace polution and conflicts with libraries that use $ */
+$(document).ready(function() {
+    if ($.fn.galleryView !== undefined) {
+        var $gallery_view = $('#galleryView');
+        if ($gallery_view.length ) {
+            var $gallery_parent = $gallery_view.parent();
+            var $gallery_class = $gallery_parent[0].className;
+            var gallery_width = $gallery_class === 'gallery_fancybox_view' ? 640 : 617;
+            var gallery_height = $gallery_class === 'gallery_fancybox_view' ? 433 : 413;
+            $('#galleryView').galleryView({
+                panel_width: gallery_width,
+                panel_height: gallery_height,
+                frame_width: 50,
+                frame_height: 50,
+                transition_speed: 350,
+                transition_interval: 10000
+            });
+        }
+    }
+});
+}(jQuery));
