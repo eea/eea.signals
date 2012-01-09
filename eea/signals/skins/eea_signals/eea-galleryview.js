b27febf58e2fe0eb4c8ab2c64b55f43a42249374
ichim-david <ichim.david@gmail.com>
2012-01-09 14:41:31 +0000

    Refs #8827; modified width of galleryView to fit the new proportions of the portal-column-content

--- eea/signals/skins/eea_signals/eea-galleryview.js
+++ eea/signals/skins/eea_signals/eea-galleryview.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
         if ($gallery_view.length ) {
             var $gallery_parent = $gallery_view.parent();
             var $gallery_class = $gallery_parent[0].className;
-            var gallery_width = $gallery_class === 'gallery_fancybox_view' ? 640 : 617;
+            var gallery_width = $gallery_class === 'gallery_fancybox_view' ? 640 : 613;
             var gallery_height = $gallery_class === 'gallery_fancybox_view' ? 433 : 413;
             $('#galleryView').galleryView({
                 panel_width: gallery_width,
                 panel_height: gallery_height,
                 frame_width: 50,
                 frame_height: 50,
-                transition_speed: 350,
+                transition_speed: 350, 
                 transition_interval: 10000
             });
         }
