bb132dafed6f440187bf17071e5848999d47c162
ichim-david <ichim.david@gmail.com>
2018-04-11 16:33:01 +0300

    Refs #94172 avoid lazy loading of galleryview images

--- eea/signals/skins/eea_signals/eea-galleryview.js
+++ eea/signals/skins/eea_signals/eea-galleryview.js
@@ -22,19 +22,30 @@ $(document).ready(function() {
             var defaults = {
                 panel_width: gallery_width,
                 panel_height: gallery_height,
+                show_filmstrip: true,
+                show_filmstrip_nav: true,
+                fade_panel: false,
                 frame_width: 50,
                 frame_height: 50,
                 transition_speed: 350,
                 transition_interval: 10000
             };
             var options = $.extend(defaults, opts);
-            
+
             $this.galleryView(options);
             $.data(this, 'visited', 'true');
         });
 
         };
-        $("#galleryView, .galleryView").eeaGalleryView();
+        $("#galleryView, .galleryView").each(function(idx, el){
+            var opts = {};
+            if (el.className.indexOf('js-noFilmstrip') !== -1) {
+                opts['show_filmstrip'] = false;
+                opts['show_filmstrip_nav'] = false;
+            }
+           $(el).eeaGalleryView(opts);
+            // $(el).eeaGalleryView();
+        });
     }
 });
 }(jQuery));
