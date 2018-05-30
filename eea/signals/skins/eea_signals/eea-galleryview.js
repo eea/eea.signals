c8991d9053326b2b00e0e7c7c6773ec9992d6088
Alexandru Ghica <alecghica@gmail.com>
2018-05-30 14:41:56 +0000

    fixed jslint warnings, refs #95891

--- eea/signals/skins/eea_signals/eea-galleryview.js
+++ eea/signals/skins/eea_signals/eea-galleryview.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
         $("#galleryView, .galleryView").each(function(idx, el){
             var opts = {};
             if (el.className.indexOf('js-noFilmstrip') !== -1) {
-                opts['show_filmstrip'] = false;
-                opts['show_filmstrip_nav'] = false;
+                opts.show_filmstrip = false;
+                opts.show_filmstrip_nav = false;
             }
            $(el).eeaGalleryView(opts);
             // $(el).eeaGalleryView();
